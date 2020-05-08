a553b13e8183e35c7400880838dce61c92582029
Ali Parvizi <snparvizi75@gmail.com>
2020-05-08 16:47:32 +0430

    animated opacity

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ class MinjQuery {
     constructor(element) {
         this.element = element;
         this.visibale = true;
+
+        this.onGoingInterval = null;
+    }
+
+    cancelInterval() {
+        if (this.onGoingInterval != null){
+            clearInterval(this.onGoingInterval);
+        }
     }
 
     addClass(className) {
@@ -30,8 +38,62 @@ class MinjQuery {
         this.element.innerHTML = '';
     }
 
+    getOpacity() {
+        // get element opacity
+        return parseFloat(window.getComputedStyle(this.element).opacity);
+    }
+
+    setOpacity(opacity) {
+        this.element.style.opacity = opacity.toString()
+    }
+
+    checkAndSetOpacity(target, isUp){
+        let currentOpacity = this.getOpacity();
+
+        console.log(currentOpacity, target);
+
+        // clear interval if target reached
+        if (currentOpacity === target)
+            this.cancelInterval();
+
+        if (isUp)
+            this.setOpacity(currentOpacity + 0.01);
+        else
+            this.setOpacity(currentOpacity - 0.01);
+    }
+
     fadeToggle(duration) {
-        // fade toggle
+        // cancel any on going interval
+        this.cancelInterval();
+
+        // initial setup
+        duration = duration * 1000;                 // convert to millisecond
+        let initialOpacity = this.getOpacity();     // get the initial opacity
+
+        let totalOpacityStepsNeeded;                // total number of intervals needed to get to target opacity
+        let timeFactor;                             // amount of time to get to target opacity
+        let timeQuantum;                            // interval between each opacity update call
+        let target;                                 // target opacity
+
+        if (this.visibale) {
+            target = 0;
+            totalOpacityStepsNeeded = parseInt(initialOpacity * 100 );
+            timeFactor = duration * totalOpacityStepsNeeded/100;
+            timeQuantum = timeFactor / (totalOpacityStepsNeeded);
+
+        } else {
+            target = 1;
+            totalOpacityStepsNeeded = parseInt((1 - initialOpacity) * 100 );
+            timeFactor = duration * totalOpacityStepsNeeded/100;
+            timeQuantum = timeFactor / (totalOpacityStepsNeeded);
+        }
+
+        this.onGoingInterval = setInterval(()=> {
+            this.checkAndSetOpacity(target, this.visibale);
+        }, timeQuantum);
+
+        // toggle internal visibility state
+        this.visibale = !this.visibale;
     }
 
     hasClass(className) {
