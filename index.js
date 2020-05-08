244d50e8cb75f9e4967f87e4eab9373b0bede259
Ali Parvizi <snparvizi75@gmail.com>
2020-05-08 14:32:28 +0430

    MinjQuery structure

---
+++ index.js
@@ -0,0 +1,44 @@
+class MinjQuery {
+    constructor(element) {
+    }
+
+    addClass(className) {
+        // add call to element
+    }
+
+    attr(attrName) {
+        // get attr value of the elemnt
+    }
+
+    children() {
+        // return a list of children of the element
+    }
+
+    click(callback) {
+        // set on click listener with callback
+    }
+
+    empty() {
+        // remove all children
+    }
+
+    fadeToggle(duration) {
+        // fade toggle
+    }
+
+    hasClass(className) {
+        // Determine whether passed element has given class
+    }
+
+    hover(callback) {
+        // hover call back
+    }
+
+    removeAttr(attrName) {
+        // remove attr
+    }
+
+    toggleClass(className) {
+        // toggle class
+    }
+}
